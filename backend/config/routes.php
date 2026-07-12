464e7418d36fa8a5995ae140534fb86e9b264638
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-12 02:46:13 -0300

    fix: backend - corrigindo rota de pacientes para passar parametros corretamente

--- backend/config/routes.php
+++ backend/config/routes.php
@@ -39,9 +39,9 @@ return function (RouteBuilder $routes): void {
         
         $builder->get('/pacientes', ['controller' => 'Pacientes', 'action' => 'index']);
         $builder->post('/pacientes', ['controller' => 'Pacientes', 'action' => 'add']);
-        $builder->get('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'view']);
-        $builder->put('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'edit']);
-        $builder->delete('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'delete']);
+        $builder->get('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'view'])->setPass(['id']);;
+        $builder->put('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'edit'])->setPass(['id']);;
+        $builder->delete('/pacientes/{id}', ['controller' => 'Pacientes', 'action' => 'delete'])->setPass(['id']);;
 
     });
 
