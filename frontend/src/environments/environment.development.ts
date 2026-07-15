f94d7021a0af02e98ee1d02b5832e7955b6589dd
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-15 20:57:23 -0300

    feat: frontend - adicionando arquivo de environments

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiAtendimentos: 'https://localhost:8765/api'
+};
---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiAtendimentos: 'https://localhost:8765/api'
+};
