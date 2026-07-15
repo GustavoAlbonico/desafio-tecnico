3282052eba3733e518a56cb095e0d6b1f62f39d9
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-14 21:01:52 -0300

    feat: backend - criando projeto + estrutura base em angular

---
+++ frontend/src/app/features/atendimentos/atendimento-form/atendimento-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atendimento-form',
+  imports: [],
+  templateUrl: './atendimento-form.html',
+  styleUrl: './atendimento-form.scss',
+})
+export class AtendimentoForm {}
---
+++ frontend/src/app/shared/components/table-list/table-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list',
+  imports: [],
+  templateUrl: './table-list.html',
+  styleUrl: './table-list.scss',
+})
+export class TableList {}
