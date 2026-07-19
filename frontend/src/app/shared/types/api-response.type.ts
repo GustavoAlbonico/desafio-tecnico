798b834759f673ca7d3b3a42618e4b981ea7ec3f
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-19 18:44:15 -0300

    fix: frontend - corrigindo interfaces de response da API de atendimentos

--- frontend/src/app/shared/types/api-response.type.ts
+++ frontend/src/app/shared/types/api-response.type.ts
@@ -1,22 +1,22 @@
-import { Pagination } from "./pagination.type";
+import { PaginationApi } from "./pagination.type";
 
 export interface ApiResponseBase {
-  success: boolean;
-  message: string;
-  errors?: ApiErrors | null;
+  success: boolean,
+  message: string,
+  errors?: ApiErrors | null,
 }
 
-export interface ApiListResponse<T> extends ApiResponseBase {
-  data?: T[] | null;
-  pagination?: Pagination;
+export interface ApiPaginatedResponse<T> extends ApiResponseBase {
+  data?: T[] | null,
+  pagination?: PaginationApi,
 }
 
-export interface ApiItemResponse<T> extends ApiResponseBase {
-  data?: T | null;
+export interface ApiResponse<T> extends ApiResponseBase {
+  data?: T | null,
 }
 
 export interface ApiErrors {
   [campo: string]: {
-    [regra: string]: string;
-  };
+    [regra: string]: string,
+  },
 }
