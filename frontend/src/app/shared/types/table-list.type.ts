0a0f4bba6f56127dd4ce745ef0d7bc69266ee05a
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-19 18:30:48 -0300

    feat: frontend - criando componente dinâmico de table e suas interfaces

---
+++ frontend/src/app/shared/types/table-list.type.ts
@@ -0,0 +1,34 @@
+import { SortDirection } from "@angular/material/sort"
+
+export interface TableListSettings<T> {
+  reference: string,
+  columnName: string,
+  value: (item: T) => string | number,
+  align?: 'left' | 'center' | 'right',
+  pipe?: TableColumnPipe
+}
+
+export interface TableAction {
+  name: string,
+  icon: string,
+  label: string,
+}
+
+export interface TableActionEvent<T> {
+  name: string,
+  item: T,
+}
+
+export interface TableSortSettings {
+  sort: string,
+  direction: SortDirection
+}
+
+export type TableColumnPipe =
+  | {type: 'text',}
+  | {type: 'date',format?: string}
+  | {type: 'datetime',format?: string}
+  | {type: 'currency',currencyCode?: string}
+  | {type: 'percent',digitsInfo?: string}
+  | {type: 'number',digitsInfo?: string}
+  | {type: 'boolean',trueLabel?: string,falseLabel?: string};
