10efbc156a7f7c844fcd06314851b2d064ba19c2
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-07-19 18:51:16 -0300

    feat: frontend - adicionando constant para opções do enum de status

--- frontend/src/app/features/atendimentos/constants/atendimento.constant.ts
+++ frontend/src/app/features/atendimentos/constants/atendimento.constant.ts
@@ -1,11 +1,12 @@
 import { StatusAtendimento } from "../enums/status-atendimento.enum";
 
-
 export const STATUS_ATENDIMENTO_LABELS: Record<StatusAtendimento, string> = {
   [StatusAtendimento.Agendado]: 'Agendado',
   [StatusAtendimento.Confirmado]: 'Confirmado',
   [StatusAtendimento.Cancelado]: 'Cancelado',
 };
+
+export const STATUS_ATENDIMENTO_VALUES = Object.values(StatusAtendimento).filter(value => typeof value === 'number') as StatusAtendimento[];
 
 export const ATENDIMENTO_SORT_FIELD_MAP: Record<string, string> = {
   id: 'id',
